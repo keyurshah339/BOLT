a659705c030ddbaa9e553eca02500fd9f1cbcb6c
keyurshah339 <75299295+keyurshah339@users.noreply.github.com>
2022-02-21 13:53:29 +0530

    refactor

--- functionalities/hamburgerMenuToggle.js
+++ functionalities/hamburgerMenuToggle.js
@@ -1,31 +1,31 @@
 
 
 
-//Doc Menu Sidebar
-let docMenu = document.querySelector("#hamburgerBttn-doc");
-let sideBar = document.querySelector(".components-nav");
-let toggleSideBar = document.querySelector(".components-nav-sidebar");
+  //Doc Menu Sidebar
+  let docMenu = document.querySelector("#hamburgerBttn-doc");
+  let sideBar = document.querySelector(".components-nav");
+  let toggleSideBar = document.querySelector(".components-nav-sidebar");
 
-if (docMenu && sideBar) {
-  docMenu.addEventListener("click", () => {
-    sideBar.classList.toggle("components-nav-sidebar");
-  });
-}
+  if (docMenu && sideBar) {
+    docMenu.addEventListener("click", () => {
+      sideBar.classList.toggle("components-nav-sidebar");
+    });
+  }
 
-// hide nav
-function hideSideNavBar(){
-  sideBar.classList.toggle("components-nav-sidebar");
-}
+    // hide nav
+    function hideSideNavBar(){
+      sideBar.classList.toggle("components-nav-sidebar");
+    }
 
 
-//bolt Menu Sidebar
-let boltMenu = document.querySelector("#hamburgerBttn-bolt");
-let boltSideBar = document.querySelector(".nav-list");
+    //bolt Menu Sidebar
+    let boltMenu = document.querySelector("#hamburgerBttn-bolt");
+    let boltSideBar = document.querySelector(".nav-list");
 
-if (boltMenu && boltSideBar) {
-  boltMenu.addEventListener("click", () => {
-    boltSideBar.classList.toggle("nav-list-sidebar");
-  });
-}
+    if (boltMenu && boltSideBar) {
+      boltMenu.addEventListener("click", () => {
+      boltSideBar.classList.toggle("nav-list-sidebar");
+    });
+  }
 
 
